670af77f6440f100e6e73033c0aba5337d452386
wonCha25 <chawj1234@gmail.com>
2025-08-23 08:22:42 +0900

    chore: Create iOS project

---
+++ SyncTank-iOS/SyncTank-iOS/SyncTank_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncTank_iOSApp.swift
+//  SyncTank-iOS
+//
+//  Created by 차원준 on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncTank_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyncTank-iOS/SyncTank-iOSTests/SyncTank_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyncTank_iOSTests.swift
+//  SyncTank-iOSTests
+//
+//  Created by 차원준 on 8/23/25.
+//
+
+import Testing
+@testable import SyncTank_iOS
+
+struct SyncTank_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
